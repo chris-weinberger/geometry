237549781ebab1e84384335130b9ddcefaabaac5
Chris Weinberger <cleeweinberger@gmail.com>
2025-07-29 20:49:57 +0000

    add files to repo, initial commit

---
+++ src/geometry/circle.py
@@ -0,0 +1,21 @@
+def calculate_area(radius):
+    """
+    Calculate area of a circle.
+
+    Parameters
+    ----------
+    radius : float
+        The radius of a circle.
+
+    Returns
+    -------
+    area : float
+        The area calculated.
+    """
+    return pi*blob_radius ** 2
+
+def calculate_circ(radius):
+    """
+    Calculate circumference of a circle
+    """
+    return 2 * pi * radius
